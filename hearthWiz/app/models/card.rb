37a3eff32005cc1043609ff57f0699a673f91eef
burythehammer <matt@burythehammer.com>
2015-03-10 13:47:16 +0000

    Some validations added to card model

--- hearthWiz/app/models/card.rb
+++ hearthWiz/app/models/card.rb
@@ -4,9 +4,51 @@ class Card < ActiveRecord::Base
 		presence: true
 	
 	validates :cost, 
-		presence: true
+		presence: true,
+		:numericality => { :only_integer => true }
 
 	validates :card_type,
-		presence: true
+		presence: true,
+		:inclusion => { :in => ["Minion", "Spell", "Weapon", "Enchantment"] }
+
+
+	validates :rarity_id,
+		presence: true,
+		:numericality => { :only_integer => true }
+	
+	validates :faction,
+	:inclusion => { :in => ["Alliance", "Horde", "Neutral"] }
+
+=begin
+	validates :text
+
+	validates :mechanics
+
+	validates :flavour
+
+	validates :artist
+=end
+
+
+	validates :attack,
+	:numericality => { :only_integer => true }
+
+	validates :health,
+	:numericality => { :only_integer => true }
+
+	validates :collectible,
+	:inclusion => {:in => [true, false]}
+
+	validates :json_id,
+	presence: true,
+	uniqueness: true
+
+	validates :elite,
+	:inclusion => {:in => [true, false]}
+
+	validates :durability,
+	:numericality => { :only_integer => true }
+
+#	validates :how_to_get_gold
 
 end
