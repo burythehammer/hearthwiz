8a4720a54d0d431b2607ac782aece56006933286
burythehammer <matt@burythehammer.com>
2015-03-10 13:28:52 +0000

    The index page now sorts by mana cost rather than alphabetically.

--- hearthWiz/app/controllers/cards_controller.rb
+++ hearthWiz/app/controllers/cards_controller.rb
@@ -1,18 +1,17 @@
 class CardsController < ApplicationController
-  before_action :set_card, only: [:update, :destroy]
 
   # GET /cards
   # GET /cards.json
   def index
-    @cards = Card.all
+    @cards = Card.all.sort_by(&:cost)
   end
 
   # GET /cards/1
   # GET /cards/1.json
   def show
     begin
-      set_card
-    rescue ActiveRecord::RecordNotFound
+      set_card 
+    rescue ActiveRecord::RecordNotFound 
       flash[:error] = 'That card could not be found!'
       redirect_to action: "index"
     end
@@ -23,11 +22,37 @@ class CardsController < ApplicationController
     def set_card
         @card = Card.find(params[:id])
         @rarity = Rarity.find(@card.rarity_id)
+        @quickInfo = getCardQuickInfo
     end
-    
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def card_params
       params.require(:card).permit(:name, :cost, :rarity)
+    end
+
+
+    def getCardQuickInfo
+      quickInfo = Hash.new
+
+      quickInfo[:cost] = @card.cost
+      quickInfo[:health] = @card.health if minion?
+      quickInfo[:attack] = @card.attack if minion?
+      quickInfo[:rarity] = @rarity.name
+      quickInfo[:disenchant] = @rarity.disenchant_value if disenchantable?
+#      quickInfo[] = @card.
+#      quickInfo[:durability] = @card.
+    end
+
+    def weapon?
+      return @card.card_type == "Weapon"
+    end
+
+    def minion?
+      return @card.card_type == "Minion"
+    end
+
+    def disenchantable?
+#      TODO
     end
 
 end
