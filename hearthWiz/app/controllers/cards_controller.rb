f0598fcd74dfeb38efb6a12d26cd4488c58bc269
burythehammer <matt@burythehammer.com>
2015-03-11 11:33:58 +0000

    Refactored login in controller/view back to the card model. Lots of helper methods added to card model. Prettified seed console output.

--- hearthWiz/app/controllers/cards_controller.rb
+++ hearthWiz/app/controllers/cards_controller.rb
@@ -22,38 +22,12 @@ class CardsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_card
         @card = Card.find(params[:id])
-        @rarity = Rarity.find(@card.rarity_id)
-        @quickInfo = getCardQuickInfo
+        @quickInfo = @card.getCardQuickInfo
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def card_params
       params.require(:card).permit(:name, :cost, :rarity)
-    end
-
-
-    def getCardQuickInfo
-      quickInfo = Hash.new
-
-      quickInfo[:cost] = @card.cost
-      quickInfo[:health] = @card.health if minion?
-      quickInfo[:attack] = @card.attack if minion?
-      quickInfo[:rarity] = @rarity.name
-      quickInfo[:disenchant] = @rarity.disenchant_value if disenchantable?
-#      quickInfo[] = @card.
-#      quickInfo[:durability] = @card.
-    end
-
-    def weapon?
-      return @card.card_type == "Weapon"
-    end
-
-    def minion?
-      return @card.card_type == "Minion"
-    end
-
-    def disenchantable?
-#      TODO
     end
 
 end
--- hearthWiz/app/models/card.rb
+++ hearthWiz/app/models/card.rb
@@ -3,6 +3,9 @@ class Card < ActiveRecord::Base
 	belongs_to :rarities
 	validates_associated :rarities
 
+	belongs_to :player_classes
+	validates_associated :player_classes
+
 
 	validates :name, 
 		presence: true
@@ -60,6 +63,58 @@ class Card < ActiveRecord::Base
 
 
 #	validates :how_to_get_gold
+
+  	def weapon?
+  	    return :card_type == "Weapon"
+    end
+
+    def minion?
+  	    return :card_type == "Minion"
+    end
+
+    def disenchantable?
+		return !self.getDisenchantValue.nil?
+    end
+
+    def craftable?
+    	return !self.getCraftCost.nil?
+    end
+
+    def getRarityName
+      	return self.getRarity.name
+    end
+
+    def getRarityColour
+    	return self.getRarity.colour
+    end
+
+    def getRarityHexColour
+    	return self.getRarity.hexcolour
+    end
+
+    def getDisenchantValue
+    	return self.getRarity.disenchant_reward
+    end
+
+	def getCraftCost
+    	return self.getRarity.craft_cost
+    end
+
+	def getRarity
+		return Rarity.find(self.rarity_id)
+	end
+
+    def getCardQuickInfo
+		quickInfo = Hash.new
+
+		quickInfo[:cost] = self[:cost]
+		quickInfo[:health] = self[:health] if minion?
+		quickInfo[:attack] = self[:attack] if minion?
+		quickInfo[:rarity] = self.getRarityName
+		quickInfo[:disenchant] = self.getDisenchantValue if disenchantable?
+		quickInfo[:craft] = self.getCraftCost if craftable?
+		quickInfo[:durability] = self.durability if weapon?
+    end
 
 
 end
