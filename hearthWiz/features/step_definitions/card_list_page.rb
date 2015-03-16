aa429f19e03b683fd71b748ca877d54c015fbb96
burythehammer <matt@burythehammer.com>
2015-03-16 17:53:00 +0000

    Some more cucumber additions

--- hearthWiz/features/step_definitions/card_list_page.rb
+++ hearthWiz/features/step_definitions/card_list_page.rb
@@ -15,3 +15,21 @@ Then(/^I should see the card list page$/) do
 	expect(page).to have_css('div#all_cards_title', :text == "All Cards")
 end
 
+Then(/^the card list page tells me '(.*)' costs '(\d+)' mana$/) do |card_name, mana_cost|
+#	expect(page).to have_css('div#all_cards_title', :text == "#{mana_cost}")
+pending
+end
+
+Then(/^the card list page tells me '(.*)' has '(\d+)' health$/) do |card_name, health|
+#	expect(page).to have_css('div#all_cards_title', :text == "#{health}")
+pending
+end
+
+Then(/^the card list page tells me '(.*)' has '(\d+)' attack$/) do |card_name, attack|
+#	expect(page).to have_css('div#', :text == "#{attack}")
+pending
+end
+
+Then(/^I should see the '(.*)' card listed$/) do |card_name|
+	expect(page).to have_css("div##{card_name}")
+end
