8ee8a8950931c3008c424d540f2517ec5f5cf58d
burythehammer <matt@burythehammer.com>
2015-03-24 12:54:03 +0000

    Seeding is now environment dependent. Changed JSON ID column limit to 100 characters as the previous limit was too restrictive.

---
+++ hearthWiz/db/seeds/production.rb
@@ -0,0 +1,7 @@
+#loads entire helpers directory
+Dir[File.join(Rails.root, 'db', 'helpers', '*.rb')].each {|file| require file }
+
+clean_database
+seed_rarities
+seed_classes
+seed_production_cards
