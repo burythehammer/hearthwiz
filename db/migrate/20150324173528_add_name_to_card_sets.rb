ef9d603ed5aa9544ebdb4b46fffe867b81c93109
burythehammer <matt@burythehammer.com>
2015-03-27 14:21:32 +0000

    Some fixes to migration files

--- db/migrate/20150324173528_add_name_to_card_sets.rb
+++ db/migrate/20150324173528_add_name_to_card_sets.rb
@@ -1,9 +1,5 @@
 class AddNameToCardSets < ActiveRecord::Migration
   def change
-<<<<<<< HEAD
-  	add_column :card_sets, :name, :string, :limit => 100
-=======
   	add_column :card_sets, :name, :string, limit: 100
->>>>>>> 3225faf8f7fcad636fb1c65f8d1179238b185fd1
   end
 end
