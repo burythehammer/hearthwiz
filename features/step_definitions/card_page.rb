2f971637376534b1c7b6cdcadca82fba371c9fa2
burythehammer <matt@burythehammer.com>
2015-04-17 23:17:56 +0100

    Cucumber tests have been fixed

--- features/step_definitions/card_page.rb
+++ features/step_definitions/card_page.rb
@@ -8,7 +8,7 @@ When(/^I open the '(.+)' card page$/) do |card_name|
 end
 
 Then(/^I should be on the '(.+)' card page$/) do |card_name|
-	expect(page).to have_css('div#card_title', :text == card_name)
+	expect(page).to have_css('div.page-title', :text == card_name)
 end
 
 Then(/^I should be on the path for the card with the id '(.+)'$/) do |card_id|
@@ -16,33 +16,33 @@ Then(/^I should be on the path for the card with the id '(.+)'$/) do |card_id|
 end
 
 Then(/^the card page tells me it is the type '(.+)'$/) do |card_type|
-	expect(page).to have_css('div#card_type', :text == card_type)
+	expect(page).to have_css('div#card-type', :text == card_type)
 end
 
 Then(/^the card page displays the card text '(.*)'$/) do |card_text|
-	expect(page).to have_css('div#card_text', :text == card_text)  
+	expect(page).to have_css('div#card-text', :text == card_text)  
 end
 
 Then(/^the card page displays the flavour text '(.*)'$/) do |flavour_text|
-	expect(page).to have_css('div#card_flavour_text', :text == flavour_text)  
+	expect(page).to have_css('div#card-flavour-text', :text == flavour_text)  
 end
 
 Then(/^the card page tells me it costs '(\d+)' mana$/) do |mana|
-	expect(page).to have_css('div#card_mana_cost', :text == mana)  
+	expect(page).to have_css('div#card-mana-cost', :text == mana)  
 end
 
 Then(/^the card page tells me it has '(\d+)' health$/) do |health|
-	expect(page).to have_css('div#card_health', :text == health)  
+	expect(page).to have_css('div#card-health', :text == health)  
 end
 
 Then(/^the card page tells me it has '(\d+)' durability$/) do |durability|
-	expect(page).to have_css('div#card_durability', :text == durability)  
+	expect(page).to have_css('div#card-durability', :text == durability)  
 end
 
 Then(/^the card page tells me it has '(\d+)' attack$/) do |attack|
-  expect(page).to have_css('div#card_attack', :text == attack)  
+  expect(page).to have_css('div#card-attack', :text == attack)  
 end
 
-Then(/^the card page tells me it is the rarity '(.+)'$/) do |rarity|
-  expect(page).to have_css('div#card_rarity', :text == rarity)  
+Then(/^the page subtitle tells me it is the rarity '(.+)'$/) do |rarity|
+  expect(page).to have_css('div.page-subtitle', :text == rarity)  
 end
