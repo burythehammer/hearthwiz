404128150c4a7ff2d0cc1ecd6f2894ca7e5144cb
burythehammer <matt@burythehammer.com>
2015-05-08 17:14:04 +0100

    Updates to player class model

--- app/models/player_class.rb
+++ app/models/player_class.rb
@@ -1,2 +1,15 @@
 class PlayerClass < ActiveRecord::Base
+  has_many :cards
+  validates :name, presence: true, uniqueness: true
+  validates :hero, uniqueness: true, length: { maximum: 50 }
+
+  validates :colour,
+            presence: true,
+            length: { maximum: 50 }
+
+  validates :hexcolour,
+            presence: true,
+            length: { is: 7 }
+
+  validates_format_of :hexcolour, with: /\A(#[A-Fa-f0-9]{6})\z/
 end
