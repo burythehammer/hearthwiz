3c2fc0a047e0d0996543f329d30cdc6860e1873a
burythehammer <matt@burythehammer.com>
2015-05-16 00:15:52 +0100

    Fixing Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@
 # This model is maintained through the Devise gem.
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
