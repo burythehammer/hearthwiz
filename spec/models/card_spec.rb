5b4de06d26657c97309290eda4bce718238065ed
burythehammer <matt@burythehammer.com>
2015-05-15 12:13:46 +0100

    Card Model rspec example additions

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,11 +1,46 @@
 require 'spec_helper'
 
 RSpec.describe Card, type: :model do
-  it 'should be collectible' do
-    card = Card.new
-    card.collectible = true
-    card.should be_collectible
+  context ':json_id' do
+    it 'should have a json id'
   end
 
-  it 'should have a rarity'
+  context ':player_class' do
+    it 'should have a player class'
+    it 'should default to neutral player class'
+  end
+
+  context ':card_set' do
+    it 'should have a card set'
+  end
+
+  context ':card_type' do
+    it 'should have a card type'
+
+    context ':minion' do
+      it 'should have a mana cost'
+      it 'should have an attack value'
+      it 'should have a health value'
+    end
+
+    context ':spell' do
+      it 'should have a mana cost'
+    end
+
+    context ':weapon' do
+      it 'should have a mana cost'
+      it 'should have an attack value'
+      it 'should have a durability which is numeric'
+    end
+  end
+
+  context ':rarity' do
+    it 'should have a rarity'
+    it 'should default to common rarity'
+  end
+
+  context ':collectible' do
+    it 'should be collectible'
+  end
+
 end
