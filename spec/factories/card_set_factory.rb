2c1c91d3bea0810d02ab7e13abc1563a56d77295
burythehammer <matt@burythehammer.com>
2015-06-11 11:10:37 +0100

    Factorygirl fixes - card factory now populates from other factories properly

--- spec/factories/card_set_factory.rb
+++ spec/factories/card_set_factory.rb
@@ -1,6 +1,6 @@
 # spec/factories/card_set_factory.rb
 FactoryGirl.define do
-  factory :card_set do
+  factory :card_set, aliases: [:fake_card_set] do
     name { Faker::Lorem.word }
   end
 end
--- spec/factories/player_class_factory.rb
+++ spec/factories/player_class_factory.rb
@@ -1,6 +1,6 @@
 # spec/factories/player_class_factory.rb
 FactoryGirl.define do
-  factory :player_class do
+  factory :player_class, aliases: [:fake_player_class] do
     name { Faker::Lorem.word }
     colour { Faker::Lorem.word }
     hexcolour { '#' + Faker::Number.number(6) }
